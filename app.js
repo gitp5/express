3defdb144723a95202c35c6ba75fb2df42d11966
hosohiro <hiroshi_hosokawa@cyber-u.ac.jp>
2024-11-27 19:01:33 +0900

    add hello page

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-//app.use('/hello', helloRouter);
+app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
 app.use('/cat', catRouter);
 
