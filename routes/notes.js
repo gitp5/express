cfe08015533af9c4cac91a584b8084f0de25063e
hosohiro <hosohiro@mac.com>
2025-07-19 10:31:07 +0900

    add env

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,17 @@
 var express= require('express');
 var router= express.Router();
+const cors = require('cors');
+require('dotenv').config();
+
 // 接続情報を設定
 const{ MongoClient} = require("mongodb");
-const uri= "*****";
+//const uri= "*****";
+const uri= process.env.MONGODB_URI;
 const client= new MongoClient(uri);
+
+// corsミドルウェアを使用
+router.use(cors());
+
 router.get('/', async(req, res) =>{
 // データベース、コレクションを指定
 const database= client.db('notes');
